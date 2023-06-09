1490307c8a9189e805c060f0658d03a4a5b8b674
김주원 <juwon5814@gmail.com>
2023-06-09 22:37:57 +0900

    [INIT] 초기 템플릿 생성

---
+++ src/main/java/com/example/bobblejump/BobblejumpApplication.java
@@ -0,0 +1,13 @@
+package com.example.bobblejump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobblejumpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BobblejumpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bobblejump/BobblejumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bobblejump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobblejumpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
